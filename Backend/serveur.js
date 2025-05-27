d138f171c0dc780d0bc0ed65512117411901fee6
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-27 18:14:50 +0100

    Configuration du port

--- Backend/serveur.js
+++ Backend/serveur.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
+
 
 app.listen(PORT, () => {
   console.log(` Notre Serveur backend sur http://localhost:${PORT}`);

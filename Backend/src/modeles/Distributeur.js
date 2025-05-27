304eee268b0bf9212b4f888b019a7e8b13348f31
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-27 17:06:47 +0100

    creation de classe

--- Backend/src/modeles/Distributeur.js
+++ Backend/src/modeles/Distributeur.js
@@ -1,3 +1,19 @@
 /**
  * Classe représentant le distributeur automatique et sa logique métier
  */
+
+class Distributeur {
+  constructor() {
+    // Initialisation des propriétés
+    this.solde = 0; // Solde actuel de l'utilisateur en MAD
+    this.produits = [ // Liste des produits disponibles
+      { id: 1, nom: "Soda", prix: 3.5, quantite: 10 },
+      { id: 2, nom: "Chips", prix: 2.0, quantite: 15 },
+      { id: 3, nom: "Bonbon", prix: 1.5, quantite: 20 },
+      { id: 4, nom: "Eau", prix: 2.5, quantite: 12 }
+    ];
+    this.panier = []; // Produits sélectionnés par l'utilisateur
+    this.piecesAcceptees = [0.5, 1, 2, 5, 10]; // Pièces acceptées par la machine
+  }
+  
+}

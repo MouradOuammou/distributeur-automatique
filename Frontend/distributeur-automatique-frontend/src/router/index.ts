66a58de64076cac80f4753842616a7a957f03539
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-27 20:07:56 +0100

    Création  un fichier de déclaration globale

--- Frontend/distributeur-automatique-frontend/src/router/index.ts
+++ Frontend/distributeur-automatique-frontend/src/router/index.ts
@@ -1,10 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DistributeurView from '@/views/DistributeurView.vue'
+import ConfirmationView from '@/views/ConfirmationView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'distributeur',
+    component: DistributeurView
+  },
+  {
+    path: '/confirmation',
+    name: 'confirmation',
+    component: ConfirmationView,
+    props: route => ({ transaction: route.state?.transaction })
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-
-  ],
+  history: createWebHistory(),
+  routes
 })
 
 export default router

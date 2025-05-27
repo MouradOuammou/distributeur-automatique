20c2bc2ee3193b898ebffaea3797afccabe3ebd0
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-27 18:15:45 +0100

    Lancement du serveur

--- Backend/serveur.js
+++ Backend/serveur.js
@@ -5,4 +5,7 @@ const HOST = process.env.HOST || '0.0.0.0';
 
 app.listen(PORT, () => {
   console.log(` Notre Serveur backend sur http://localhost:${PORT}`);
+    console.log(`Serveur Backend démarré sur http://${HOST}:${PORT}`);
+     console.log(`Environnement: ${process.env.NODE_ENV || 'development'}`);
+        console.log(`=================================`);
 });

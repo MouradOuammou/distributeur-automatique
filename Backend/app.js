3dc90a72a0c1785ad06e212440c55293d38af348
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-27 18:09:52 +0100

    config express app avec cors, json parser et logging des requêtes

--- Backend/app.js
+++ Backend/app.js
@@ -1,21 +1,12 @@
+/**
+ * Fichier principal de configuration de l'application Express
+ * Configure :
+ * - Les middlewares
+ * - Les routes
+ * - La gestion d'erreurs
+ */
+
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-
-const app = express();
-
-// Middlewares
-app.use(cors());
-app.use(bodyParser.json());
-
-// Routes
-app.use('/api', require('./routes/distributeurRoutes'));
-
-// Gestion des erreurs
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ erreur: 'Erreur  du serveur' });
-});
-
-module.exports = app;
+const path = require('path');

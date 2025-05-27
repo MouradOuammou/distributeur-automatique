884cff5731ab0ae69a18621b3b87ab5793a587ef
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-27 19:45:34 +0100

    // --- ÉTAT ---

---
+++ Frontend/distributeur-automatique-frontend/src/stores/distributeur.js
@@ -0,0 +1,13 @@
+
+import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+import api from '@/services/api'
+
+
+// Définition du store Pinia pour gérer l'état du distributeur automatique
+
+
+export const useDistributeurStore = defineStore('distributeur', () => {
+
+
+};

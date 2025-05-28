0dafdc45920eb37b47b1b63b049b1a13caa47ddb
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-28 21:29:09 +0100

    Erreurs d'import

--- Frontend/distributeur-automatique-frontend/vite.config.ts
+++ Frontend/distributeur-automatique-frontend/vite.config.ts
@@ -1,11 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -14,6 +12,15 @@ export default defineConfig({
   resolve: {
     alias: {
        '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+
     },
   },
 })

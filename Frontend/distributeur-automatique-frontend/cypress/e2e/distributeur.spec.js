baec2d7eebb33693a83da6ea0e9fe62c6bbc1ab3
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-28 22:53:27 +0100

    Ajouter test de DistributeurView

---
+++ Frontend/distributeur-automatique-frontend/cypress/e2e/distributeur.spec.js
@@ -0,0 +1,36 @@
+describe('DistributeurView', () => {
+  beforeEach(() => {
+    cy.visit('/distributeur')
+  })
+
+  it('charge la liste des produits', () => {
+    // Vérifie qu’au moins un produit est affiché dans ListeProduits
+    cy.get('[data-cy=liste-produits]')
+      .children()
+      .should('have.length.greaterThan', 0)
+  })
+
+  it('permet d’insérer une pièce', () => {
+
+    cy.get('[data-cy=inserer-piece-button]').click()
+
+    cy.get('[data-cy=montant-inseré]').should('exist').and('not.be.empty')
+  })
+
+  it('ajoute un produit au panier', () => {
+    // Clique sur un bouton "Ajouter au panier" sur un produit
+    cy.get('[data-cy=btn-ajouter-panier]').first().click()
+
+    // Vérifie que le panier contient cet item
+    cy.get('[data-cy=panier-items]').children().should('have.length.greaterThan', 0)
+  })
+
+  it('finalise un achat et navigue vers la confirmation', () => {
+    // Simule un achat
+    cy.get('[data-cy=btn-finaliser]').click()
+
+    // Soit la navigation vers confirmation
+    cy.url().should('include', '/confirmation')
+
+  })
+})
